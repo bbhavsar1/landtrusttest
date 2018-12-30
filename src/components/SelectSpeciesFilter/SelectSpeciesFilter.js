593a46d6b6ec723189831cd53d1cb24977752e97
dirtyleftboot <fduny001@gmail.com>
2018-12-30 00:25:55 -0500

    [SelectSpeciesFilter] styling #38

--- src/components/SelectSpeciesFilter/SelectSpeciesFilter.js
+++ src/components/SelectSpeciesFilter/SelectSpeciesFilter.js
@@ -3,54 +3,98 @@ import { func, string } from 'prop-types';
 import { injectIntl } from 'react-intl';
 import Select from 'react-select';
 import css from './SelectSpeciesFilter.css';
-import { bigGameTypes, fishTypes, smallGameTypes, uplandGameTypes, waterfowlTypes } from '../../marketplace-custom-config';
+import {
+  bigGameTypes, fishTypes, smallGameTypes, uplandGameTypes, waterfowlTypes,
+  ltBorderColor, ltFocusBorderColor, ltForegroundColor, ltBackgroundColor, ltActiveBackgroundColor,
+  ltActiveForegroundColor, ltHoverForegroundColor
+} from '../../marketplace-custom-config';
 
 const defaultStyle = {
-  width: '200px',
   fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
   fontSize: '12px',
   color: '#000000',
 };
 
 const cssStyles = {
-  control: styles => ({
+  control: (styles, state) => ({
     ...styles,
     ...defaultStyle,
+    width: '200px',
     margin: '4px 4px 4px 0px',
     minHeight: '30px',
     height: '30px',
-    borderColor: '#eeeeee',
+    boxShadow: 'none', // removes the blue border
+    borderColor: state.hasValue ? ltActiveBackgroundColor : ltBorderColor,
+    '&:hover': {
+      borderColor: state.hasValue ? ltActiveBackgroundColor : ltFocusBorderColor
+    },
+    backgroundColor: state.hasValue ? ltActiveBackgroundColor : ltBackgroundColor
+  }),
+  valueContainer: (styles, state) => ({
+    ...styles,
+    ...defaultStyle,
+    padding: '0px 0px 0px 6px',
   }),
   option: (styles, { data, isDisabled, isFocused, isSelected }) => {
     return {
       ...styles,
       ...defaultStyle,
-      padding: '0px 0px 0px 10px'
+      padding: '0px 0px 0px 8px',
+      backgroundColor: isSelected ? ltActiveBackgroundColor : ltBackgroundColor,
+      color: isSelected ? ltActiveForegroundColor : ltForegroundColor,
+      '&:hover': {
+        textTransform: 'uppercase',
+        color: ltHoverForegroundColor,        
+        fontWeight: 'bold'
+      },
+      '&:active': {
+        backgroundColor: ltActiveBackgroundColor,
+        color: ltActiveForegroundColor,
+      },
     };
   },
-  input: styles => ({
-    ...styles
+  input: (styles, state) => ({
+    ...styles,
+    ...defaultStyle,
+    padding: '0px',
+    color: state.hasValue ? ltActiveForegroundColor : ltForegroundColor
   }),
   placeholder: styles => ({
     ...styles,
     ...defaultStyle,
-    fontWeight: 600
+    fontWeight: 600,
   }),
-  group: (styles, { data }) => ({
+  groupHeading: (styles, { data }) => ({
     ...styles,
     ...defaultStyle,
+    fontWeight: 600,
+    padding: '0px 0px 0px 8px',
   }),
   singleValue: (styles, { data }) => ({
     ...styles,
-    fontWeight: 600
+    ...defaultStyle,
+    fontWeight: 600,
+    color: ltActiveForegroundColor
   }),
-  clearIndicator: (styles) => ({
+  clearIndicator: (styles, state) => ({
     ...styles,
     padding: '2px',
+    color: ltActiveForegroundColor,
+    '&:hover': {
+      color: ltActiveForegroundColor
+    },
   }),
-  dropdownIndicator: (styles) => ({
+  dropdownIndicator: (styles, state) => ({
     ...styles,
     padding: '2px',
+    color: state.hasValue ? ltActiveForegroundColor : ltForegroundColor,
+    '&:hover': {
+      color: state.hasValue ? ltActiveForegroundColor : ltForegroundColor
+    },
+  }),
+  indicatorSeparator: (styles, state) => ({
+    ...styles,
+    backgroundColor: state.hasValue ? ltActiveForegroundColor : ltForegroundColor
   }),
   loadingMessage: (styles) => ({
     ...styles,
--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -1,72 +1,79 @@
 /*
  * Marketplace specific configuration.
  */
+export const ltForegroundColor = '#000000';
+export const ltBackgroundColor = '#ffffff';
+export const ltBorderColor = '#EEEEEE';
+export const ltFocusBorderColor = '#CBCBCB';
+export const ltHoverForegroundColor = '#fd3713';
+export const ltActiveBackgroundColor = '#fd3713';
+export const ltActiveForegroundColor = '#ffffff';
 
 export const fishTypes = [
-  {key: 'salmon', label: 'Salmon'},
-  {key: 'black_crappie', label: 'Black crappie'},
-  {key: 'blue_gill', label: 'Bluegill'},
-  {key: 'catfish', label: 'Catfish'},
-  {key: 'trout', label: 'Trout'},
-  {key: 'carp', label: 'Carp'},
-  {key: 'largemouth_bass', label: 'Largemouth bass'},
-  {key: 'mountain_whitefish', label: 'Mountain Whitefish'},
-  {key: 'muskey', label: 'Muskey'},
-  {key: 'norther_pike', label: 'Northern Pike'},
-  {key: 'suckers', label: 'Suckers'},
-  {key: 'smallmouth_bass', label: 'Smallmouth Bass'},
-  {key: 'steelhead', label: 'Steelhead'},
-  {key: 'striped_bass', label: 'Striped Bass'},
-  {key: 'walleye', label: 'Walleye'},
-  {key: 'white_bass', label: 'White Bass'},
-  {key: 'yellow_perch', label: 'Yellow Perch'},
+  { key: 'salmon', label: 'Salmon' },
+  { key: 'black_crappie', label: 'Black crappie' },
+  { key: 'blue_gill', label: 'Bluegill' },
+  { key: 'catfish', label: 'Catfish' },
+  { key: 'trout', label: 'Trout' },
+  { key: 'carp', label: 'Carp' },
+  { key: 'largemouth_bass', label: 'Largemouth bass' },
+  { key: 'mountain_whitefish', label: 'Mountain Whitefish' },
+  { key: 'muskey', label: 'Muskey' },
+  { key: 'norther_pike', label: 'Northern Pike' },
+  { key: 'suckers', label: 'Suckers' },
+  { key: 'smallmouth_bass', label: 'Smallmouth Bass' },
+  { key: 'steelhead', label: 'Steelhead' },
+  { key: 'striped_bass', label: 'Striped Bass' },
+  { key: 'walleye', label: 'Walleye' },
+  { key: 'white_bass', label: 'White Bass' },
+  { key: 'yellow_perch', label: 'Yellow Perch' },
 ]
 
-export const bigGameTypes =[
-  {key: 'black_bear', label: 'Black Bear'},
-  {key: 'mountain_lion', label: 'Mountain Lion'},
-  {key: 'mule_deer', label: 'Mule Deer'},
-  {key: 'white_tailed_deer', label: 'White-tailed Deer'},
-  {key: 'black_tailed_deer', label: 'Black-tailed Deer'},
-  {key: 'elk', label: 'Elk'},
-  {key: 'moose', label: 'Moose'},
-  {key: 'Wolf', label: 'Wolf'},
+export const bigGameTypes = [
+  { key: 'black_bear', label: 'Black Bear' },
+  { key: 'mountain_lion', label: 'Mountain Lion' },
+  { key: 'mule_deer', label: 'Mule Deer' },
+  { key: 'white_tailed_deer', label: 'White-tailed Deer' },
+  { key: 'black_tailed_deer', label: 'Black-tailed Deer' },
+  { key: 'elk', label: 'Elk' },
+  { key: 'moose', label: 'Moose' },
+  { key: 'Wolf', label: 'Wolf' },
 ]
 
 export const smallGameTypes = [
-  {key: 'wild_pigs', label: 'Wild Pigs'},
-  {key: 'javelina', label: 'Javelina'},
-  {key: 'prairie_dogs', label: 'Prairie Dogs'},
-  {key: 'cotton_tail_rabbits', label: 'Cottontail Rabbits'},
-  {key: 'jackrabbits', label: 'Jackrabbits'},
-  {key: 'racoons', label: 'Racoons'},
-  {key: 'squirrels', label: 'Squirrels'},
-  {key: 'coyotes', label: 'Coyotes'},
+  { key: 'wild_pigs', label: 'Wild Pigs' },
+  { key: 'javelina', label: 'Javelina' },
+  { key: 'prairie_dogs', label: 'Prairie Dogs' },
+  { key: 'cotton_tail_rabbits', label: 'Cottontail Rabbits' },
+  { key: 'jackrabbits', label: 'Jackrabbits' },
+  { key: 'racoons', label: 'Racoons' },
+  { key: 'squirrels', label: 'Squirrels' },
+  { key: 'coyotes', label: 'Coyotes' },
 ]
 
-export const uplandGameTypes= [
-  {key: 'mourning_doves', label: 'Mourning Doves'},
-  {key: 'eurasian_doves', label: 'Eurasian Doves'},
-  {key: 'pigeons', label: 'Pigeons'},
-  {key: 'blue_grouse', label: 'Blue Grouse'},
-  {key: 'dusky_grouse', label: 'Dusky Grouse'},
-  {key: 'short_tailed_grouse', label: 'Sharp-tailed Grouse'},
-  {key: 'ruffed_grouse', label: 'Ruffed Grouse'},
-  {key: 'sage_grouse', label: 'Sage Grouse'},
-  {key: 'spruce_grouse', label: 'Spruce Grouse'},
-  {key: 'hungarian_partriges', label: 'Hungarian Partridges'},
-  {key: 'chukar', label: 'Chukar'},
-  {key: 'ring_necked_pheasants', label: 'Ring-necked Pheasants'},
-  {key: 'quail', label: 'Quail'},
-  {key: 'turkeys', label: 'Turkeys'},
+export const uplandGameTypes = [
+  { key: 'mourning_doves', label: 'Mourning Doves' },
+  { key: 'eurasian_doves', label: 'Eurasian Doves' },
+  { key: 'pigeons', label: 'Pigeons' },
+  { key: 'blue_grouse', label: 'Blue Grouse' },
+  { key: 'dusky_grouse', label: 'Dusky Grouse' },
+  { key: 'short_tailed_grouse', label: 'Sharp-tailed Grouse' },
+  { key: 'ruffed_grouse', label: 'Ruffed Grouse' },
+  { key: 'sage_grouse', label: 'Sage Grouse' },
+  { key: 'spruce_grouse', label: 'Spruce Grouse' },
+  { key: 'hungarian_partriges', label: 'Hungarian Partridges' },
+  { key: 'chukar', label: 'Chukar' },
+  { key: 'ring_necked_pheasants', label: 'Ring-necked Pheasants' },
+  { key: 'quail', label: 'Quail' },
+  { key: 'turkeys', label: 'Turkeys' },
 ]
 
 export const waterfowlTypes = [
-  {key: 'ducks', label: 'Ducks'},
-  {key: 'canada_geese', label: 'Canada Geese'},
-  {key: 'snow_geese', label: 'Snow Geese'},
-  {key: 'ross_geese', label: 'Ross Geese'},
-  {key: 'white_fronted_geese', label: 'White-fronted Geese'},
+  { key: 'ducks', label: 'Ducks' },
+  { key: 'canada_geese', label: 'Canada Geese' },
+  { key: 'snow_geese', label: 'Snow Geese' },
+  { key: 'ross_geese', label: 'Ross Geese' },
+  { key: 'white_fronted_geese', label: 'White-fronted Geese' },
 ]
 
 export const agricultureTypes = [
