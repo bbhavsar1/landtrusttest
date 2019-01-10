4d3b803cc51bb2895eb7ee1d117331b38077037f
dirtyleftboot <fduny001@gmail.com>
2019-01-10 01:27:43 -0500

    revert until we can figure out why we can only do per night #53

--- src/config.js
+++ src/config.js
@@ -35,7 +35,7 @@ const bookingProcessAlias = 'preauth-with-nightly-booking/release-1';
 //
 // Note: translations will use different translation keys for night, day or unit
 // depending on the value chosen.
-const bookingUnitType = 'line-item/day';
+const bookingUnitType = 'line-item/night';
 
 // Should the application fetch available time slots (currently defined as
 // start and end dates) to be shown on listing page.
