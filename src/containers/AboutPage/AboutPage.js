e88deab649da823bba3fec7298b00b55bafd164b
dirtyleftboot <fduny001@gmail.com>
2019-01-24 22:08:18 -0500

    [AboutPage] #69

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -1,113 +1,184 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { injectIntl, intlShape } from 'react-intl';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
 import config from '../../config';
-import { twitterPageURL } from '../../util/urlHelpers';
-import { StaticPage, TopbarContainer } from '../../containers';
+import ownership from './images/ownership.jpg';
+import purpose from './images/purpose.jpg';
+
+import { FormattedMessage } from 'react-intl';
 import {
+  Page,
   LayoutSingleColumn,
   LayoutWrapperTopbar,
   LayoutWrapperMain,
   LayoutWrapperFooter,
   Footer,
-  ExternalLink,
 } from '../../components';
 
+import { lazyLoadWithDimensions } from '../../util/contextHelpers';
+import { TopbarContainer } from '../../containers';
+
 import css from './AboutPage.css';
-import river from './river.jpg';
-import nic from './nic.jpg';
-import aaron from './aaron.jpg';
 
-const AboutPage = () => {
-  const { siteTwitterHandle, siteFacebookPage } = config;
-  const siteTwitterPage = twitterPageURL(siteTwitterHandle);
+export const AboutPageComponent = props => {
+  const { intl, scrollingDisabled } = props;
 
-  // prettier-ignore
+  // Schema for search engines (helps them to understand what this page is about)
+  // http://schema.org
+  // We are using JSON-LD format
+  const siteTitle = config.siteTitle;
+  const schemaTitle = intl.formatMessage({ id: 'AboutPage.schemaTitle' }, { siteTitle });
+  const schemaDescription = intl.formatMessage({ id: 'AboutPage.schemaDescription' });
+  const schemaImage = `${config.canonicalRootURL}${ownership}`;
+
+  class LocationImage extends Component {
+    render() {
+      const { alt, ...rest } = this.props;
+      return <img alt={alt} {...rest} />;
+    }
+  }
+
+  class EmailSectionStepContent extends Component {
+    render() {
+      const { titleId, textId, email } = this.props;
+      const emailLink = 'mailto:' + email;
+      return <div className={css.sectionStepDescriptionContainerThirds}>
+        <h2 className={css.sectionStepTitle}>
+          <FormattedMessage id={titleId} />
+        </h2>
+        <div className={css.sectionStepDescriptionText}>
+          <FormattedMessage id={textId} /><a className={css.email} href={emailLink} target="_top">{email}</a>
+        </div>
+      </div>
+    }
+  }
+
+  const LazyImage = lazyLoadWithDimensions(LocationImage);
+
   return (
-    <StaticPage
-      title="About Us"
+    <Page
+      className={css.root}
+      scrollingDisabled={scrollingDisabled}
+      contentType="website"
+      description={schemaDescription}
+      title={schemaTitle}
       schema={{
         '@context': 'http://schema.org',
-        '@type': 'AboutPage',
-        description: 'About LandTrust',
-        name: 'About page',
+        '@type': 'WebPage',
+        description: schemaDescription,
+        name: schemaTitle,
+        image: [schemaImage],
       }}
     >
       <LayoutSingleColumn>
         <LayoutWrapperTopbar>
           <TopbarContainer />
         </LayoutWrapperTopbar>
-
-        <LayoutWrapperMain className={css.staticPageWrapper}>
-          <div className={css.imageWrapper}>
-            <img className={css.coverImage} src={river} alt="river" />
-            <h1 className={css.pageTitle}>Who We Are</h1>
-          </div>
-          <div className={css.contentWrapper}>
-            <div className={css.contentMain}>
-              <h2>
-                Ownership
-              </h2>
-
-              <p>
-                Private land ownership is a cornerstone principle of the American dream
-                and one which we strongly support. We believe that by working with
-                conservation-minded Landowners and responsible Sportsmen we’ll be able
-                to build sustainable model to ensure conservation through the use of
-                private lands.
-              </p>
-
-              <h3 className={css.subtitle}>Landowners</h3>
-
-              <p>
-                LandTrust can’t exist without conservation-minded Landowners who
-                believe that entrusting responsible Sportsmen with harvesting fish and
-                game from their land is essential to preserving fish, game, and the
-                rich history of the American outdoorsman. If you’re a Landowner that
-                fits that bill, whether you own 5 acres or 500,000 acres, we’d enjoy
-                having you join our website. We’re in the process of launching and if
-                you register we’ll also be seeking your guidance along the way as we
-                build this company.
-              </p>
-
-              <h2 id="contact">Our Team</h2>
-              <img className={css.contentImage} src={nic} alt="nic" />
-              <h3 className={css.subtitle}>Nic De Castro</h3>
-
-              <p>
-                Nic is a sales leader in the marketing technology industry with a
-                breadth of experience both building businesses and selling products to
-                the largest advertisers on the planet. He's lived in Boston, LA,
-                Chicago, NYC, SF, Boulder, and Bozeman and has nearly broken 1M miles
-                for work travel, but has been fortunate to sneak quite a bit of fun
-                into those miles over the years. Nic grew up bird hunting and fishing
-                in Southern California from a very young age. His love for the outdoors
-                has led him to Montana where he's pursuing his passion for hunting and
-                fishing as well as building businesses that feed his lifestyle.
-              </p>
-
-              <img className={css.contentImage} src={aaron} alt="aaron" />
-              <h3 className={css.subtitle}>Aaron Ricchio</h3>
-
-              <p>
-                Aaron is a beach kid at heart who found himself in NYC in order to
-                further his design career. While working at some of the largest
-                agencies he ironically landed a position creating with Cabela's.
-                This sparked a number of relationships and fun times with the outdoor
-                retailer. Now, after over twelve years in one of the least outdoor
-                places he's begun working on his hunting and fishing skills. If he's
-                not creating ads, branding or logos, you can typically find him at
-                the airport headed off to his next climbing related adventure with
-                his badass wife.
-              </p>
+        <LayoutWrapperMain>
+          <div className={css.heroContainer}>
+            <div className={css.heroContent}>
+              <h1 className={css.heroMainTitle}>
+                <FormattedMessage id="AboutPage.heroTitle" />
+              </h1>
             </div>
           </div>
+          <ul className={css.sections}>
+            <li>
+              <div className={css.sectionContent}>
+                <div className={css.sectionTitle}>
+                  <FormattedMessage id="AboutPage.section1Title" />
+                </div>
+                <div className={css.sectionSteps}>
+                  <div className={css.sectionStepReverse}>
+                    <div className={css.sectionStepDescriptionContainer}>
+                      <h2 className={css.sectionStepTitle}>
+                        <FormattedMessage id="AboutPage.section1Part1Title" />
+                      </h2>
+                      <div className={css.sectionStepDescriptionText}>
+                        <FormattedMessage id="AboutPage.section1Part1Text" />
+                      </div>
+                    </div>
+                    <div className={css.imageWrapper}>
+                      <div className={css.aspectWrapper}>
+                        <LazyImage src={purpose} alt='purpose' className={css.locationImage} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={css.sectionStep}>
+                    <div className={css.imageWrapper}>
+                      <div className={css.aspectWrapper}>
+                        <LazyImage src={ownership} alt='ownership' className={css.locationImage} />
+                      </div>
+                    </div>
+                    <div className={css.sectionStepDescriptionContainer}>
+                      <h2 className={css.sectionStepTitle}>
+                        <FormattedMessage id="AboutPage.section1Part2Title" />
+                      </h2>
+                      <div className={css.sectionStepDescriptionText}>
+                        <FormattedMessage id="AboutPage.section1Part2Text" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className={css.section}>
+              <div className={css.sectionContent}>
+                <div className={css.sectionTitle}>
+                  <FormattedMessage id="AboutPage.section2Title" />
+                </div>
+                <div className={css.sectionSteps}>
+                  <div className={css.sectionStep}>
+                    <EmailSectionStepContent titleId="AboutPage.section2Part1Title" textId="AboutPage.section2Part1Text" email="support@landtrust.co" />
+                    <EmailSectionStepContent titleId="AboutPage.section2Part2Title" textId="AboutPage.section2Part2Text" email="info@landtrust.co" />
+                    <EmailSectionStepContent titleId="AboutPage.section2Part3Title" textId="AboutPage.section2Part3Text" email="media@landtrust.co" />
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
         </LayoutWrapperMain>
-
         <LayoutWrapperFooter>
           <Footer />
         </LayoutWrapperFooter>
       </LayoutSingleColumn>
-    </StaticPage>
+    </Page>
   );
 };
+
+const { bool, object } = PropTypes;
+
+AboutPageComponent.propTypes = {
+  scrollingDisabled: bool.isRequired,
+
+  // from withRouter
+  history: object.isRequired,
+  location: object.isRequired,
+
+  // from injectIntl
+  intl: intlShape.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    scrollingDisabled: isScrollingDisabled(state),
+  };
+};
+
+// Note: it is important that the withRouter HOC is **outside** the
+// connect HOC, otherwise React Router won't rerender any Route
+// components since connect implements a shouldComponentUpdate
+// lifecycle hook.
+//
+// See: https://github.com/ReactTraining/react-router/issues/4671
+const AboutPage = compose(
+  withRouter,
+  connect(mapStateToProps),
+  injectIntl
+)(AboutPageComponent);
 
 export default AboutPage;
